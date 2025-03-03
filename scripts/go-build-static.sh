395978bb1837d086297603d8fb14fcc4d1d5fd03
Priyanka Saggu <priyankasaggu11929@gmail.com>
2025-03-03 18:07:38 +0530

    Update go-build-static.sh

--- scripts/go-build-static.sh
+++ scripts/go-build-static.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 export CGO_ENABLED=${CGO_ENABLED:-1}
-export GOEXPERIMENT=boringcrypto
+# TODO(psaggu): uncomment once boringcrypto is available
+# export GOEXPERIMENT=boringcrypto
 
 if [ "${CGO_ENABLED}" != "1" ]; then
   echo "CGO_ENABLED=${CGO_ENABLED}, should be set to 1 for static goboring compilation" >&2
